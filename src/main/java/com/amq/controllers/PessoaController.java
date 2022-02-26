7be7c01d1300c90715b6ca7d0fb1006723d00f66
Fabricio Junior <fabriciojuniorsc@gmail.com>
2022-02-26 18:29:15 -0300

    SwaggerUI e Get Pessoa

--- src/main/java/com/amq/controllers/PessoaController.java
+++ src/main/java/com/amq/controllers/PessoaController.java
@@ -2,11 +2,11 @@ package com.amq.controllers;
 
 import com.amq.consumers.TopicoPessoaSender;
 import com.amq.models.Pessoa;
+import com.amq.repository.PessoaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/pessoa")
@@ -15,8 +15,16 @@ public class PessoaController {
     @Autowired
     private TopicoPessoaSender topicoOrigemSender;
 
+    @Autowired
+    private PessoaRepository repository;
+
     @PostMapping
     public void salvar(@RequestBody Pessoa pessoa) {
         topicoOrigemSender.send(pessoa);
+    }
+
+    @GetMapping
+    public List<Pessoa> listar() {
+        return repository.findAll();
     }
 }
